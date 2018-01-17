18f6670303a84b8b469aa7983faa130ca566d8cd
Aditya <Aditya@aditya-mac-2.dhcp.wustl.edu>
2018-01-17 14:02:49 -0600

    done with heartbeat.java

--- src/studio0/Heartbeat.java
+++ src/studio0/Heartbeat.java
@@ -1,5 +1,22 @@
 package studio0;
 
 public class Heartbeat {
+	public static void main (String[]args) {
+		int x = 1;
+		
+		while (x > 0) {
+			
+			System.out.println(x + " seconds have passed");
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
 
+				e.printStackTrace();
+			}
+			
+			x += 1;
+		}
+		
+	}
 }
